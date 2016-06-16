86224282c98dbe466d84dba8b885b8f77e101e57
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-06-16 12:03:09 -0700

    Rename Django project

--- project_solstice/project_solstice/urls.py
+++ project_solstice/project_solstice/urls.py
@@ -1,4 +1,4 @@
-"""daycare_finder URL Configuration
+"""project_solstice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
