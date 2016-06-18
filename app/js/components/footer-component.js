33505cffa7f1a5e995e2aa26346ec32f73ee06cd
Mike Skalandunas <mskalandunas@gmail.com>
2016-06-17 18:58:51 -0700

    finish footer

--- app/js/components/footer-component.js
+++ app/js/components/footer-component.js
@@ -7,7 +7,7 @@ const FooterComponent = React.createClass({
       <section className="footer is-primary" style={{paddingBottom: '40px', backgroundColor: '#8a8acb'}}>
         <div className="container">
           <div className="columns">
-            <div className="column">
+            <div className="column is-2">
               <ul className="footer-list-header">
                 <li>Schools &amp; Daycares</li>
                 <li>Features</li>
@@ -17,16 +17,16 @@ const FooterComponent = React.createClass({
                 <li>Daycare Resources</li>
               </ul>
             </div>
-            <div className="column">
+            <div className="column is-2">
               <ul className="footer-list-header">
                 <li>About</li>
-                <li>Team</li>
-                <li>Careers</li>
+                <li>Our Team</li>
+                <li>Work at Baby Dump</li>
                 <li>Press</li>
-                <li>Blog</li>
+                <li>Visit Our Blog</li>
               </ul>
             </div>
-            <div className="column">
+            <div className="column is-4">
               <ul className="footer-list-header">
                 <li>Baby Dump</li>
                 <p>7485 Cherry Blossom Ln.</p>
@@ -35,19 +35,27 @@ const FooterComponent = React.createClass({
                 <p><a href="mailto:hello@babydump.io" target="_blank">hello@babydump.io</a></p>
               </ul>
             </div>
-            <div className="column">
+            <div className="column is-2 has-text-centered">
               <ul className="footer-icon-list">
                 <li>
-                  <i className="fa fa-github"></i>
+                  <a href="https://github.com" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
                 </li>
                 <li>
-                  <i className="fa fa-facebook"></i>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <i className="fa fa-facebook"></i>
+                  </a>
                 </li>
                 <li>
-                  <i className="fa fa-instagram"></i>
+                  <a href="https://www.instagram.com" target="_blank">
+                    <i className="fa fa-instagram"></i>
+                  </a>
                 </li>
                 <li>
-                  <i className="fa fa-twitter"></i>
+                  <a href="https://www.twitter.com" target="_blank">
+                    <i className="fa fa-twitter"></i>
+                  </a>
                 </li>
               </ul>
             </div>
@@ -55,10 +63,10 @@ const FooterComponent = React.createClass({
         </div>
 
         <div className="container" style={{paddingTop: '40px'}}>
-          <div className="content is-mobile" style={{textAlign: 'center', fontWeight: '700'}}>
+          <div className="content is-mobile" style={{textAlign: 'center'}}>
             <span className="text-color-faded">©2016 Made by <a className="text-color-white" href="https://github.com/dzwell" target="_blank">Daniel</a>
             , <a className="text-color-white" href="https://github.com/flegald" target="_blank">David</a>, and&nbsp;
-            <a className="text-color-white" href="http://mikeskalandunas.com" target="_blank">Mike</a>.
+            <a className="text-color-white" href="https://mikeskalandunas.com" target="_blank">Mike</a>.
             </span>
           </div>
         </div>
