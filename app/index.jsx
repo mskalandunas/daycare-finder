23115d67cbdc9034bc0b5cd019a60a116bd044ab
Mike Skalandunas <mskalandunas@gmail.com>
2016-06-19 13:01:47 -0700

    add sign in page

--- app/index.jsx
+++ app/index.jsx
@@ -9,6 +9,7 @@ import HeaderComponent from './js/components/header-component';
 import FooterComponent from './js/components/footer-component';
 import SectionComponent from './js/components/section-component';
 import SignUpComponent from './js/components/sign-up-component';
+import SignInComponent from './js/components/sign-in-component';
 
 // Scripts
 import './js/scripts/search';
@@ -20,8 +21,11 @@ const App = () => (
     <FooterComponent/>
     */}
 
-    {/* Sign In */}
+    {/* Sign In
     <SignUpComponent/>
+    */}
+
+    <SignInComponent/>
   </div>
 );
 
