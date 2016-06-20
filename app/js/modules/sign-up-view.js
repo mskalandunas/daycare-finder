b44c9da5c6b698cb3db03d7ec0f8273c60451ad1
Mike Skalandunas <mskalandunas@gmail.com>
2016-06-19 22:21:09 -0700

    add input focus improvements

--- app/js/modules/sign-up-view.js
+++ app/js/modules/sign-up-view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router';
 
 const SignUpView = React.createClass({
   render: () => {
@@ -23,35 +24,28 @@ const SignUpView = React.createClass({
                   <h2 className="subtitle">Try us out.</h2>
                 </div>
               </div>
-
               <div className="columns">
                 <div className="column is-4"></div>
                 <div className="column is-4">
-
-
-                    <p className="control is-expanded">
-                      <input className="input input-large input-no-border" type="text" placeholder="address@email.com"/>
-                    </p>
-
+                  <p className="control is-expanded">
+                    <input className="input input-large input-no-border input-is-primary-focus" type="text" placeholder="address@email.com"/>
+                  </p>
                   <div className="control is-horizontal">
                     <div className="control is-grouped">
                       <p className="control is-expanded">
-                        <input className="input input-large input-no-border" type="password" placeholder="••••••••"/>
+                        <input className="input input-large input-no-border input-is-primary-focus" type="password" placeholder="••••••••"/>
                       </p>
                       <p className="control is-expanded">
-                        <input className="input input-large input-no-border" type="password" placeholder="Confirm Password"/>
+                        <input className="input input-large input-no-border input-is-primary-focus" type="password" placeholder="Confirm Password"/>
                       </p>
                     </div>
                   </div>
-
                   <a className="button is-primary is-fullwidth" href="#">
                     <span>Join</span>
                   </a>
-
+                  <h2 className="subtitle" style={{paddingTop: '20px'}}>Have an account already?  <Link to="/sign-in">Sign in</Link>.</h2>
                 </div>
               </div>
-
-
             </div>
           </div>
         </header>
