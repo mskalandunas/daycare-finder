3d25b7c590647bae81b07b584e558e70a93ccac8
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-06-29 16:19:56 -0700

    Add UserProfile tests

--- project_solstice/userprofile/models.py
+++ project_solstice/userprofile/models.py
@@ -7,11 +7,15 @@ from django.conf import settings
 class UserProfile(md.Model):
     """Extending Django User Object."""
 
-    user = md.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+    user = md.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name='profile',
+        on_delete=md.CASCADE
+    )
     favorites = md.ManyToManyField(Daycare, related_name='favorited')
 
     def __str__(self):
         """Return string."""
-        return self.user.username
+        return self.user
 
 
--- project_solstice/userprofile/tests.py
+++ project_solstice/userprofile/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import UserProfile
+from django.conf import settings
+import factory
 
-# Create your tests here.
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """Create User test model."""
+
+    class Meta:
+        """Meta."""
+
+        model = settings.AUTH_USER_MODEL
+
+
+class UserTestCase(TestCase):
+    """Test User and UserProfile models."""
+
+    def setUp(self):
+        """Setup."""
+        self.roger = UserFactory.create(
+            username='roger'
+        )
+
+    def test_user_profile(self):
+        """Test profile is created with user."""
+        profiles = UserProfile.objects.all()
+        self.assertIn(self.roger.profile, profiles)
+
