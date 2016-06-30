ff4b641e6ef63221097add0597f91cd35fe49e6f
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-06-30 15:05:08 -0700

    Register signals.py in apps.py

--- project_solstice/userprofile/apps.py
+++ project_solstice/userprofile/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UserprofileConfig(AppConfig):
     name = 'userprofile'
+
+    def ready(self):
+        """Tell Django to import signals when app is loaded."""
+        import userprofile.signals
